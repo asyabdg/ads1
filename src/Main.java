879db89a4735bf96a8b238e3f22179292fdbcbd0
Assemay Bakhadir <assemaybakhadir@MacBook-Pro-Assemay.local>
2024-04-09 11:04:53 +0500

    asya

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello world!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number from 1 to 10");
+        int p = scanner.nextInt();
+        int n;
+        double startTime, endTime, duration;
+        int[] arr;
+        switch(p) {
+            case 1:
+                System.out.println("Enter the length of the array:");
+                n = scanner.nextInt();
+                System.out.println("Enter the array:");
+                arr = new int[n];
+                for (int i = 0; i < n; i++) {
+                    arr[i] = scanner.nextInt();
+                }
+                startTime = System.nanoTime();
+                int result1 = find_min(arr, n);
+                endTime = System.nanoTime();
+                duration = (endTime - startTime) / 1000000;
+                System.out.println("The minimum number of the array is " + result1);
+                System.out.println("Time taken: " + duration + " milliseconds");
         }
     }
+
+    /** * This method finds the minimum number of the array
+     * It uses an iterative approach *
+     * Time complexity: O(n), where n is the length of the array
+     * resulting in linear time complexity
+     * @param arr The array for which the minimum is to be found
+     * @param n The length of the array arr
+     * @return The minimun of the array */
+    public static int find_main(int[] arr, int n) {
+        if (arr.length == 0) {
+            return 0;
+        }
+        int min = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+
 }
